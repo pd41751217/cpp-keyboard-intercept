3abc65b2eb58fb12f8a1918e787fa0ea1ad1e1b8
best coder <pd04170501@gmail.com>
2025-09-26 09:51:56 +0200

    Test 'Bob Block Adventures'

--- apps/app-electron/src/GAME_TO_HOOK.ts
+++ apps/app-electron/src/GAME_TO_HOOK.ts
@@ -1,11 +1,11 @@
 import { Hook } from './hook/apps-manager';
 
-// export const GAME_TO_HOOK: Hook = {
-//   id: 'bobblockadventures',
-//   windowName: 'Bob Block Adventures',
-// };
-
 export const GAME_TO_HOOK: Hook = {
-  id: 'cs1.6',
-  windowName: 'Counter-Strike',
+  id: 'bobblockadventures',
+  windowName: 'Bob Block Adventures',
 };
+
+// export const GAME_TO_HOOK: Hook = {
+//   id: 'cs1.6',
+//   windowName: 'Counter-Strike',
+// };
